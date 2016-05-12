58b30e6fe622bde46d9281f9c301effbddd6a671
TomW1605 <tom.w.1605@gmail.com>
2016-05-12 20:01:41 +0800

    popup done

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 var badIDs = [1663, 1608, 1110, 1350, 1506, 1525, 1193];
 
-var permanentLinkToggle = true;
-var imageLinkToggle = true;
-var explainLinkToggle = true;
-var Link3DToggle = true;
+chrome.storage.local.get(['permanentLinkToggle', 'imageLinkToggle', 'explainLinkToggle', 'Link3DToggle', 'titleTextMover', 'keyboardNavigation'], function (data) {
+    var permanentLinkToggle = data['permanentLinkToggle'];
+    var imageLinkToggle = data['imageLinkToggle'];
+    var explainLinkToggle = data['explainLinkToggle'];
+    var Link3DToggle = data['Link3DToggle'];
+    var titleTextMover = data['titleTextMover'];
+    var keyboardNavigation = data['keyboardNavigation'];
 
-var id = document.querySelectorAll(".comicNav")[0].children[1].children[0].href;
-id = id.replace(/^([\s\S]*)(com\/)/g, '');
-id = id.replace(/(\/)([\s\S]*)$/g, '');
-console.log(id);
-id = parseInt(id)+1;
-
-if(!(badIDs.indexOf(id)>=0)) {
-    document.onkeydown = checkNav;
-
+    var id = document.querySelectorAll(".comicNav")[0].children[1].children[0].href;
+    id = id.replace(/^([\s\S]*)(com\/)/g, '');
+    id = id.replace(/(\/)([\s\S]*)$/g, '');
+    console.log(id);
+    id = parseInt(id) + 1;
 
     function checkNav(e) {
         e = e || window.event;
         var comicNav = document.querySelectorAll(".comicNav");
-        if (comicNav.length == 0) {
+        if (comicNav.length === 0) {
             return;
         }
         if (e.keyCode == '37') {
@@ -34,65 +33,71 @@ if(!(badIDs.indexOf(id)>=0)) {
         }
     }
 
-
-    document.addEventListener('load', function () {
-        // not every comic img is in the comic div, sometimes you have links or other stuff
-        // recurse down the DOM starting from comic until you hit an <img>
-        var findComic = function (div) {
-            var children = div.children;
-            for (var i = 0; i < children.length; i++) {
-                var child = children[i];
-                if (child.tagName === "IMG") return child;
-                else if (child.children.length > 0) return findComic(child);
-            }
-            return null;
-        };
-        var comic = document.getElementById("comic");
-        if (comic === null) {
-            return;
+    if (!(badIDs.indexOf(id) >= 0)) {
+        if(keyboardNavigation) {
+            document.onkeydown = checkNav;
         }
-        if (window.titleText === undefined) {
-            window.titleText = true;
-            var image = findComic(comic);
-            var paragraph = document.createElement("p");
-            paragraph.innerHTML = 'Title text: '+image.title;
-            paragraph.id = "titleText";
-            var padding = "25px";
-            paragraph.style.paddingLeft = padding;
-            paragraph.style.paddingRight = padding;
-            comic.appendChild(paragraph);
+
+        if(titleTextMover) {
+            document.addEventListener('load', function () {
+                // not every comic img is in the comic div, sometimes you have links or other stuff
+                // recurse down the DOM starting from comic until you hit an <img>
+                var findComic = function (div) {
+                    var children = div.children;
+                    for (var i = 0; i < children.length; i++) {
+                        var child = children[i];
+                        if (child.tagName === "IMG") return child;
+                        else if (child.children.length > 0) return findComic(child);
+                    }
+                    return null;
+                };
+                var comic = document.getElementById("comic");
+                if (comic === null) {
+                    return;
+                }
+                if (window.titleText === undefined) {
+                    window.titleText = true;
+                    var image = findComic(comic);
+                    var paragraph = document.createElement("p");
+                    paragraph.innerHTML = 'Title text: ' + image.title;
+                    paragraph.id = "titleText";
+                    var padding = "25px";
+                    paragraph.style.paddingLeft = padding;
+                    paragraph.style.paddingRight = padding;
+                    comic.appendChild(paragraph);
+                }
+            }, true);
         }
-    }, true);
 
+        var theDiv = document.getElementById("middleContainer");
+        var html = theDiv.outerHTML;
 
-    var theDiv = document.getElementById("middleContainer");
-    var html = theDiv.outerHTML;
+        var imageURL = html;
+        imageURL = imageURL.replace(/^([\s\S]*)(hotlinking\/embedding\): )/g, '');
+        imageURL = imageURL.replace(/(<div id="transcript")([\s\S]*)$/g, '');
 
-    var imageURL = html;
-    imageURL = imageURL.replace(/^([\s\S]*)(hotlinking\/embedding\): )/g, '');
-    imageURL = imageURL.replace(/(<div id="transcript")([\s\S]*)$/g, '');
+        var permanentLink = 'Permanent link to this comic: http://xkcd.com/' + id + '/';
+         if(permanentLinkToggle) {
+         permanentLink = 'Permanent link to this comic: <a title="permanent link to this comic" href="http://xkcd.com/' + id + '/" target="_blank">http://xkcd.com/' + id + '/</a>';
+         }
 
-    var permanentLink = '';
-    if(permanentLinkToggle) {
-        permanentLink = 'Permanent link to this comic: <a title="permanent link to this comic" href="http://xkcd.com/' + id + '/" target="_blank">http://xkcd.com/' + id + '/</a>';
+         var imageLink = '<br>Image URL (for hotlinking/embedding): ' + imageURL;
+         if(imageLinkToggle) {
+         imageLink = '<br>Image URL (for hotlinking/embedding): <a title="link to this comic\'s image" href="' + imageURL + '" target="_blank">' + imageURL + '</a>';
+         }
+
+         var explainLink = '';
+         if(explainLinkToggle) {
+         explainLink = '<br>This comic\'s explain xkcd page: <a title="link to explain xkcd" href="http://www.explainxkcd.com/wiki/index.php/' + id + '" target="_blank">http://www.explainxkcd.com/wiki/index.php/' + id + '</a>';
+         }
+
+         var Link3D = '';
+         if (id <= 880 && Link3DToggle) {
+         Link3D = '<br>3D version of this comic: <a title="link to 3D comic" href="http://xk3d.xkcd.com/' + id + '" target="_blank">http://xk3d.xkcd.com/' + id + '</a>';
+         }
+
+         html = html.replace(/(Permanent link)([\s\S]*)((png|jpg)|http:\/\/imgs.xkcd.com\/comics\/)/, permanentLink+imageLink+explainLink+Link3D);
+
+         theDiv.outerHTML = html;/**/
     }
-
-    var imageLink = '';
-    if(imageLinkToggle) {
-        imageLink = '<br>Image URL (for hotlinking/embedding): <a title="link to this comic\'s image" href="' + imageURL + '" target="_blank">' + imageURL + '</a>';
-    }
-
-    var explainLink = '';
-    if(explainLinkToggle) {
-        explainLink = '<br>This comic\'s explain xkcd page: <a title="link to explain xkcd" href="http://www.explainxkcd.com/wiki/index.php/' + id + '" target="_blank">http://www.explainxkcd.com/wiki/index.php/' + id + '</a>';
-    }
-
-    var Link3D = '';
-    if (id <= 880 && Link3DToggle) {
-        Link3D = '<br>3D version of this comic: <a title="link to 3D comic" href="http://xk3d.xkcd.com/' + id + '" target="_blank">http://xk3d.xkcd.com/' + id + '</a>';
-    }
-
-    html = html.replace(/(Permanent link)([\s\S]*)((png|jpg)|http:\/\/imgs.xkcd.com\/comics\/)/, permanentLink+imageLink+explainLink+Link3D);
-
-    theDiv.outerHTML = html;
-}
+});
